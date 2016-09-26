98d0f082702eff5a93858dc0449ea82fa170a92b
Christian Newman <cnewman@kent.edu>
2016-09-26 18:31:55 -0400

    Updated srcml function calls in stringtosrcml

--- src/tests/TestFlatSlice.cpp
+++ src/tests/TestFlatSlice.cpp
@@ -40,28 +40,22 @@ inline char* TrimFromEnd(char *s, size_t len){
     }
     return nullptr;
 }
-std::string StringToSrcML(std::string str){
+std::string StringToSrcML(const std::string str){
     struct srcml_archive* archive;
     struct srcml_unit* unit;
-    size_t size = 0;
-
-    char *ch = new char[str.size()];
-
+    size_t size = 0;        
+    char *ch = new char[str.size()];        
     archive = srcml_archive_create();
     srcml_archive_enable_option(archive, SRCML_OPTION_POSITION);
-    srcml_archive_write_open_memory(archive, &ch, &size);
-
+    srcml_archive_write_open_memory(archive, &ch, &size);        
     unit = srcml_unit_create(archive);
-    srcml_unit_set_language(unit, SRCML_LANGUAGE_CXX);
-    srcml_unit_set_filename(unit, "testsrcSlice.cpp");
-
+    srcml_unit_set_language(unit, SRCML_LANGUAGE_CXX);        
     srcml_unit_parse_memory(unit, str.c_str(), str.size());
-    srcml_write_unit(archive, unit);
-    
+    srcml_archive_write_unit(archive, unit);            
     srcml_unit_free(unit);
     srcml_archive_close(archive);
     srcml_archive_free(archive);
-    TrimFromEnd(ch, size);
+    //TrimFromEnd(ch, size);
     return std::string(ch);
 }
 typedef std::unordered_set<std::pair<std::string, unsigned int>, NameLineNumberPairHash> cfuncset;
