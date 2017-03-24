0d18ce3eff2b677d7aeb2bd64b97f36f2a3b3357
pejordan1791 <pjordan6@kent.edu>
2017-03-24 13:47:15 -0400

    Added srcslicetest

--- src/headers/FunctionSliceProfilePolicy.hpp
+++ src/headers/FunctionSliceProfilePolicy.hpp
@@ -6,23 +6,39 @@
 #include <exception>
 #include <DeclTypePolicy.hpp>
 #include <ExprPolicy.hpp>
+#include <stack>
 
-//Problem with variables with same name in different scopes (Use a stack, every time change of scope push onto stack)
-//Possibly add line numbers to names
+using std::stack;
 
-class FunctionSliceProfilePolicy : public srcSAXEventDispatch::EventListener, public srcSAXEventDispatch::PolicyDispatcher, public srcSAXEventDispatch::PolicyListener {
+// problem with variables with same name in different scopes (Use a stack, every time change of scope push onto stack)
+// possibly add line numbers to names
+
+class FunctionSliceProfilePolicy : public srcSAXEventDispatch::EventListener, public srcSAXEventDispatch::PolicyDispatcher, public srcSAXEventDispatch::PolicyListener 
+{
     public:
-        struct FunctionSliceProfile{
+        struct FunctionSliceProfile
+        {
             FunctionSliceProfile(){}
-            FunctionSliceProfile(std::string name, std::string type, std::set<unsigned int> lines){
-                identifierName = name;
+
+            FunctionSliceProfile(std::string name, std::string type, std::set<unsigned int> lines)
+            {
+                identifierName = name + std::to_string(linenumber); // name of var and line number
                 identifierType = type;
                 scopelevel = -1;
             }
 
-            void clear(){
+            void clear() // the rest in srcSlicePolicy
+            {
                 identifierName.clear();
                 identifierType.clear();
+
+                linenumber = -1;
+                scopelevel = -1;
+
+                isConst = false;
+                isReference = false;
+                isPointer = false;
+                isStatic = false;
             }
 
             std::vector<std::string> namespaces;
@@ -41,42 +57,62 @@ class FunctionSliceProfilePolicy : public srcSAXEventDispatch::EventListener, pu
             std::set<unsigned int> def;
             std::set<unsigned int> use;
         };
-        struct FunctionSliceProfileMap{
-           FunctionSliceProfileMap() = default;
-           FunctionSliceProfileMap(std::map<std::string, FunctionSliceProfile> dat){
-            dataset = dat;
-           }
-           void clear(){
-            dataset.clear();
-           }
-           std::map<std::string, FunctionSliceProfile> dataset;
+
+        struct FunctionSliceProfileMap // the mapping of var name string and the profile
+        {
+           	FunctionSliceProfileMap() = default;
+
+           	FunctionSliceProfileMap(std::map<std::string, FunctionSliceProfile> dat)
+           	{
+            	dataset = dat;
+           	}
+           	
+           	void clear()
+           	{
+            	dataset.clear();
+           	}
+           	
+           	std::map<std::string, FunctionSliceProfile> dataset;
         };
+        
         FunctionSliceProfileMap profileset;
+
         FunctionSliceProfile data;
+
         ~FunctionSliceProfilePolicy(){}
-        FunctionSliceProfilePolicy(std::initializer_list<srcSAXEventDispatch::PolicyListener *> listeners = {}): srcSAXEventDispatch::PolicyDispatcher(listeners){
+
+        FunctionSliceProfilePolicy(std::initializer_list<srcSAXEventDispatch::PolicyListener*> listeners = {}) : srcSAXEventDispatch::PolicyDispatcher(listeners)
+        {
             currentscopelevel = 0;
-            exprPolicy.AddListener(this);
+            // adds these as listeners
+            exprPolicy.AddListener(this); 
             declTypePolicy.AddListener(this);
-            InitializeEventHandlers();
+            InitializeEventHandlers(); // handles the events
         }
 
-        void Notify(const PolicyDispatcher * policy, const srcSAXEventDispatch::srcSAXEventContext & ctx) override {
+        void Notify(const PolicyDispatcher *policy, const srcSAXEventDispatch::srcSAXEventContext &ctx) override 
+        {
             using namespace srcSAXEventDispatch;
-            if(ctx.IsOpen(ParserState::declstmt) && ctx.IsClosed(ParserState::exprstmt)){
+            
+            // "declestmt" is open and "exprestmt" is closed
+            if(ctx.IsOpen(ParserState::declstmt) && ctx.IsClosed(ParserState::exprstmt))
+            {
                 std::cerr<<"Call decl"<<std::endl;
                 declData = *policy->Data<DeclTypePolicy::DeclTypeData>();
 
-                //Generate profile
+                // generates the profile
                 auto it = profileset.dataset.find(declData.nameofidentifier);
-                if(it != profileset.dataset.end()){
+                if(it != profileset.dataset.end())
+                {
                     it->second.identifierName = declData.nameofidentifier;
                     it->second.identifierType = declData.nameoftype;
                     it->second.isConst = declData.isConst;
                     it->second.isReference = declData.isReference;
                     it->second.isPointer = declData.isPointer;
                     it->second.isStatic = declData.isStatic;
-                }else{
+                }
+                else // inserts into map if it's not there
+                {
                     data.identifierName = declData.nameofidentifier;
                     data.identifierType = declData.nameoftype;
                     data.isConst = declData.isConst;
@@ -85,57 +121,75 @@ class FunctionSliceProfilePolicy : public srcSAXEventDispatch::EventListener, pu
                     data.isStatic = declData.isStatic;
                     profileset.dataset.insert(std::make_pair(declData.nameofidentifier, data));
                 }
-            }else if (ctx.IsOpen(ParserState::exprstmt) && ctx.IsClosed(ParserState::declstmt)){
+            }
+            // "declestmt" is closed and "exprestmt" is open
+            else if(ctx.IsOpen(ParserState::exprstmt) && ctx.IsClosed(ParserState::declstmt))
+            {
                 exprData = *policy->Data<ExprPolicy::ExprDataSet>();
-                for(auto var : exprData.dataset){
+                for(auto var : exprData.dataset)
+                {
                     auto it = profileset.dataset.find(var.first);
-                    if(it != profileset.dataset.end()){
+                    if(it != profileset.dataset.end()) // if "it" hadn't reached the end, meaning the string was found
+                    {
                         it->second.def = var.second.def;
                         it->second.use = var.second.use;
-                    }else{
-                        std::cerr<<"couldn't find identifier named: "<<var.first<<std::endl;
                     }
+                    else // if the string for the var name wasn't found
+                        std::cerr<<"Couldn't find identifier named: "<<var.first<<std::endl;
                 }
             }
         }
 
     protected:
-        void * DataInner() const override {
-            //export profile to listeners
+        void *DataInner() const override 
+        {
+            // export profile to listeners
             return new FunctionSliceProfile(data);
         }
     private:
         ExprPolicy exprPolicy;
         ExprPolicy::ExprDataSet exprData;
+
         DeclTypePolicy declTypePolicy;
         DeclTypePolicy::DeclTypeData declData;
+
         int currentscopelevel;
-        void InitializeEventHandlers(){
+
+        void InitializeEventHandlers()
+        {
             using namespace srcSAXEventDispatch;
 
-            openEventMap[ParserState::declstmt] = [this](srcSAXEventContext& ctx) {
+            // opening the EventMap
+            openEventMap[ParserState::declstmt] = [this](srcSAXEventContext& ctx) 
+            {
                 ctx.dispatcher->AddListenerDispatch(&declTypePolicy);
             };
-            openEventMap[ParserState::exprstmt] = [this](srcSAXEventContext& ctx) {
+            openEventMap[ParserState::exprstmt] = [this](srcSAXEventContext& ctx) 
+            {
                 ctx.dispatcher->AddListenerDispatch(&exprPolicy);
             };
-            openEventMap[ParserState::block] = [this](srcSAXEventContext& ctx) {
+            openEventMap[ParserState::block] = [this](srcSAXEventContext& ctx) 
+            {
                 ++currentscopelevel;
             };
-            closeEventMap[ParserState::declstmt] = [this](srcSAXEventContext& ctx){
+
+            // closing the EventMap
+            closeEventMap[ParserState::declstmt] = [this](srcSAXEventContext& ctx)
+            {
                 ctx.dispatcher->RemoveListenerDispatch(&declTypePolicy);
             };
-            //Closing EventMap
-            closeEventMap[ParserState::exprstmt] = [this](srcSAXEventContext& ctx){
+            closeEventMap[ParserState::exprstmt] = [this](srcSAXEventContext& ctx)
+            {
                 ctx.dispatcher->RemoveListenerDispatch(&exprPolicy);
             };
-            closeEventMap[ParserState::block] = [this](srcSAXEventContext& ctx){
-              --currentscopelevel;  
+            closeEventMap[ParserState::block] = [this](srcSAXEventContext& ctx)
+            {
+              	--currentscopelevel;  
             };
-            closeEventMap[ParserState::function] = [this](srcSAXEventContext& ctx){
+            closeEventMap[ParserState::function] = [this](srcSAXEventContext& ctx)
+            {
                 NotifyAll(ctx);
             };
         }
-
 };
 #endif
--- src/headers/SrcSlicePolicy.hpp
+++ src/headers/SrcSlicePolicy.hpp
@@ -5,37 +5,71 @@
 #include <srcSAXHandler.hpp>
 #include <exception>
 #include <FunctionSliceProfilePolicy.hpp>
+#include <unordered_map>
 
-class SrcSlicePolicy : public srcSAXEventDispatch::EventListener, public srcSAXEventDispatch::PolicyDispatcher, public srcSAXEventDispatch::PolicyListener {
+class SrcSlicePolicy : public srcSAXEventDispatch::EventListener, public srcSAXEventDispatch::PolicyDispatcher, public srcSAXEventDispatch::PolicyListener 
+{
     public:
+        SrcSlicePolicy(std::initializer_list<srcSAXEventDispatch::PolicyListener*> listeners = {}) : srcSAXEventDispatch::PolicyDispatcher(listeners)
+        {
+            // making SSP a listener for FSPP
+            funcSliceProfilePolicy.AddListener(this);
+            InitializeEventHandlers();
+        }
+
         /*This will contain the full map*/
-        struct SliceProfileSet{
+        struct SliceProfileSet
+        {
             SliceProfileSet(){}
+
+            // map containing unique variable name key (possibly var name, line numb, func, file); tiers; return all and user can choose
+            // and profile data (small maps from FuctionSliceProfilePolicy)
+            std::unordered_map<std::string, FunctionSliceProfilePolicy::FunctionSliceProfileMap> varNameProf;
+
+            void clear()
+            {
+                varNameProf.clear();
+
+                /*
+                for(auto it = varNameProf.begin(); it != varNameProf.end(); ++it)
+                */
+            }
         };
 
-        void Notify(const PolicyDispatcher * policy, const srcSAXEventDispatch::srcSAXEventContext & ctx) override {
+        // other policies notify that finished
+        // passes back data that's casted into appropriate type into "fuctionSliceData"
+        void Notify(const PolicyDispatcher *policy, const srcSAXEventDispatch::srcSAXEventContext &ctx) override 
+        {
             using namespace srcSAXEventDispatch;
+            functionSliceData = policy->Data<FunctionSliceProfilePolicy::FunctionSliceProfileMap>();
         }
 
     protected:
-        void * DataInner() const override {
-            //export profile to listeners
+        void *DataInner() const override 
+        {
+            // export profile to listeners
             return new SliceProfileSet(data);
         }
+        
     private:
         SliceProfileSet data;
         FunctionSliceProfilePolicy funcSliceProfilePolicy;
-        FunctionSliceProfilePolicy::FunctionSliceProfileMap functionSliceData;
+        FunctionSliceProfilePolicy::FunctionSliceProfileMap *functionSliceData;
         int currentscopelevel;
-        void InitializeEventHandlers(){
+
+        void InitializeEventHandlers()
+        {
             using namespace srcSAXEventDispatch;
-            openEventMap[ParserState::function] = [this](srcSAXEventContext& ctx){
+            // add policy to listener
+            openEventMap[ParserState::function] = [this](srcSAXEventContext& ctx)
+            {
                 ctx.dispatcher->AddListenerDispatch(&funcSliceProfilePolicy);
             };
-            closeEventMap[ParserState::function] = [this](srcSAXEventContext& ctx){
+
+            closeEventMap[ParserState::function] = [this](srcSAXEventContext& ctx)
+            {
                 ctx.dispatcher->RemoveListenerDispatch(&funcSliceProfilePolicy);
             };
         }
-
 };
 #endif
