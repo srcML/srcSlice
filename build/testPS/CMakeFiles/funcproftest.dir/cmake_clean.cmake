0566e5682f9fc899bca5af63e990a2a1cccca0d9
pejordan1791 <pjordan6@kent.edu>
2017-05-05 16:39:24 -0400

    Completed test

---
+++ build/testPS/CMakeFiles/funcproftest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/funcproftest.dir/funcProfSrcSliceTest.cpp.o"
+  "../bin/funcproftest.pdb"
+  "../bin/funcproftest"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/funcproftest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ build/testPS/CMakeFiles/srcslicetest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/srcslicetest.dir/srcSliceTest.cpp.o"
+  "../bin/srcslicetest.pdb"
+  "../bin/srcslicetest"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/srcslicetest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
