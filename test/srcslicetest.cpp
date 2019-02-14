7975380b4109dd36bdc1b6ad847114f687c3f183
Christian Newman <cdnvse@rit.edu>
2019-02-14 01:41:45 -0500

    Adding CI config and dummy tests

---
+++ test/srcslicetest.cpp
@@ -0,0 +1,35 @@
+#include <srcml.h>
+#include <gtest/gtest.h>
+#include <libxml/tree.h>
+#include <libxml/parser.h>
+#include <libxml/xpath.h>
+#include <libxml/xpathInternals.h>
+
+std::string StringToSrcML(std::string str){
+    struct srcml_archive* archive;
+    struct srcml_unit* unit;
+    size_t size = 0;
+
+    char *ch = 0;
+
+    archive = srcml_archive_create();
+    srcml_archive_enable_option(archive, SRCML_OPTION_POSITION);
+    srcml_archive_write_open_memory(archive, &ch, &size);
+
+    unit = srcml_unit_create(archive);
+    srcml_unit_set_language(unit, SRCML_LANGUAGE_CXX);
+    srcml_unit_set_filename(unit, "testsrcType.cpp");
+
+    srcml_unit_parse_memory(unit, str.c_str(), str.size());
+    srcml_archive_write_unit(archive, unit);
+    
+    srcml_unit_free(unit);
+    srcml_archive_close(archive);
+    srcml_archive_free(archive);
+    ch[size-1] = 0;
+    return std::string(ch);
+}
+TEST(DummyTest, TestDummy) {
+    //Test for expected count
+    EXPECT_EQ(5, 5);
+}
