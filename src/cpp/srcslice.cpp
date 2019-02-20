a5135e51f18c98845271c56f3c138321f9344522
Christian Newman <cdnvse@rit.edu>
2019-02-20 00:16:50 -0500

    Adding code to accept file on command line

--- src/cpp/srcslice.cpp
+++ src/cpp/srcslice.cpp
@@ -1,8 +1,12 @@
 #include <srcslicepolicy.hpp>
-int main(){
+int main(int argc, char** argv){
+        if(argc < 2){
+        	std::cerr<<"Syntax: ./srcslice [srcML file name]"<<std::endl;
+        	return 0;
+        }
         std::unordered_map<std::string, std::vector<SliceProfile>> profileMap;
         SrcSlicePolicy* cat = new SrcSlicePolicy(&profileMap);
-        srcSAXController control("kdevelop.cpp.xml");
+        srcSAXController control(argv[1]);
         srcSAXEventDispatch::srcSAXEventDispatcher<> handler({cat});
         control.parse(&handler); //Start parsing
         for(auto it : profileMap){
