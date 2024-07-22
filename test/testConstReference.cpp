d6618277d81c991c6947a3e1d31b24e7e89c741a
Ender <ender@TheMachine>
2024-07-21 22:26:53 -0400

    Added more test cases for testConstReference.cpp

--- test/testConstReference.cpp
+++ test/testConstReference.cpp
@@ -92,7 +92,7 @@ TEST_CASE( "Const-Reference Test 3", "[srcslice]" ) {
             "    \"use\": [  ],\n"
             "    \"definition\": [ 1 ]\n"
             "}\n"
-            "}";
+            "}\n";
 
     testStatus = (strcmp(input.c_str(), output.c_str()) == 0);
     DebugOutput(verbose, testStatus, "Const-Reference Test 3", input, output);
@@ -132,10 +132,321 @@ TEST_CASE( "Const-Reference Test 4", "[srcslice]" ) {
             "    \"use\": [ 2 ],\n"
             "    \"definition\": [ 1 ]\n"
             "}\n"
-            "}";
+            "}\n";
 
     testStatus = (strcmp(input.c_str(), output.c_str()) == 0);
     DebugOutput(verbose, testStatus, "Const-Reference Test 4", input, output);
+
+    REQUIRE( strcmp(input.c_str(), output.c_str()) == 0 );
+}
+
+TEST_CASE( "Const-Reference Test 5", "[srcslice]" ) {
+    sourceCode = "void jar(const int& rdp, const int& rdx) {\n"
+                "    if (rdp > 5) {\n"
+                "    }\n"
+                "}";
+
+    input = FetchSlices(sourceCode, "file.cpp");
+
+    output = "{\n"
+            "\"slice_0\" : {\n"
+            "    \"file\":\"file.cpp\",\n"
+            "    \"class\":\"\",\n"
+            "    \"function\":\"jar\",\n"
+            "    \"type\":\"int\",\n"
+            "    \"name\":\"rdx\",\n"
+            "    \"dependentVariables\": [  ],\n"
+            "    \"aliases\": [  ],\n"
+            "    \"calledFunctions\": [  ],\n"
+            "    \"use\": [  ],\n"
+            "    \"definition\": [ 1 ]\n"
+            "},\n"
+            "\"slice_1\" : {\n"
+            "    \"file\":\"file.cpp\",\n"
+            "    \"class\":\"\",\n"
+            "    \"function\":\"jar\",\n"
+            "    \"type\":\"int\",\n"
+            "    \"name\":\"rdp\",\n"
+            "    \"dependentVariables\": [  ],\n"
+            "    \"aliases\": [  ],\n"
+            "    \"calledFunctions\": [  ],\n"
+            "    \"use\": [ 2 ],\n"
+            "    \"definition\": [ 1 ]\n"
+            "}\n"
+            "}\n"
+
+    testStatus = (strcmp(input.c_str(), output.c_str()) == 0);
+    DebugOutput(verbose, testStatus, "Const-Reference Test 5", input, output);
+
+    REQUIRE( strcmp(input.c_str(), output.c_str()) == 0 );
+}
+
+TEST_CASE( "Const-Reference Test 6", "[srcslice]" ) {
+    sourceCode = "void jar(const int& rdp, const int& rdx) {\n"
+                "    if (rdp > 5) {\n"
+                "        std::cout << rdp << std::endl;\n"
+                "    }\n"
+                "}";
+
+    input = FetchSlices(sourceCode, "file.cpp");
+
+    output = "{\n"
+            "\"slice_0\" : {\n"
+            "    \"file\":\"file.cpp\",\n"
+            "    \"class\":\"\",\n"
+            "    \"function\":\"jar\",\n"
+            "    \"type\":\"int\",\n"
+            "    \"name\":\"rdx\",\n"
+            "    \"dependentVariables\": [  ],\n"
+            "    \"aliases\": [  ],\n"
+            "    \"calledFunctions\": [  ],\n"
+            "    \"use\": [  ],\n"
+            "    \"definition\": [ 1 ]\n"
+            "},\n"
+            "\"slice_1\" : {\n"
+            "    \"file\":\"file.cpp\",\n"
+            "    \"class\":\"\",\n"
+            "    \"function\":\"jar\",\n"
+            "    \"type\":\"int\",\n"
+            "    \"name\":\"rdp\",\n"
+            "    \"dependentVariables\": [  ],\n"
+            "    \"aliases\": [  ],\n"
+            "    \"calledFunctions\": [  ],\n"
+            "    \"use\": [ 2, 3 ],\n"
+            "    \"definition\": [ 1 ]\n"
+            "}\n"
+            "}\n"
+
+    testStatus = (strcmp(input.c_str(), output.c_str()) == 0);
+    DebugOutput(verbose, testStatus, "Const-Reference Test 6", input, output);
+
+    REQUIRE( strcmp(input.c_str(), output.c_str()) == 0 );
+}
+
+TEST_CASE( "Const-Reference Test 7", "[srcslice]" ) {
+    sourceCode = "void jar(const int& rdp, const int& rdx) {\n"
+                "    if (rdx > 5) {\n"
+                "        std::cout << rdp << std::endl;\n"
+                "    }\n"
+                "}";
+
+    input = FetchSlices(sourceCode, "file.cpp");
+
+    output = "{\n"
+            "\"slice_0\" : {\n"
+            "    \"file\":\"file.cpp\",\n"
+            "    \"class\":\"\",\n"
+            "    \"function\":\"jar\",\n"
+            "    \"type\":\"int\",\n"
+            "    \"name\":\"rdx\",\n"
+            "    \"dependentVariables\": [  ],\n"
+            "    \"aliases\": [  ],\n"
+            "    \"calledFunctions\": [  ],\n"
+            "    \"use\": [ 2 ],\n"
+            "    \"definition\": [ 1 ]\n"
+            "},\n"
+            "\"slice_1\" : {\n"
+            "    \"file\":\"file.cpp\",\n"
+            "    \"class\":\"\",\n"
+            "    \"function\":\"jar\",\n"
+            "    \"type\":\"int\",\n"
+            "    \"name\":\"rdp\",\n"
+            "    \"dependentVariables\": [  ],\n"
+            "    \"aliases\": [  ],\n"
+            "    \"calledFunctions\": [  ],\n"
+            "    \"use\": [ 3 ],\n"
+            "    \"definition\": [ 1 ]\n"
+            "}\n"
+            "}\n"
+
+    testStatus = (strcmp(input.c_str(), output.c_str()) == 0);
+    DebugOutput(verbose, testStatus, "Const-Reference Test 7", input, output);
+
+    REQUIRE( strcmp(input.c_str(), output.c_str()) == 0 );
+}
+
+TEST_CASE( "Const-Reference Test 8", "[srcslice]" ) {
+    sourceCode = "void jar(const int& rdp, const int& rdx) {\n"
+                "    if (rdx > 5) {\n"
+                "        std::cout << rdp << std::endl;\n"
+                "    }\n"
+                "    std::cout << rdx + rdp << std::endl;\n"
+                "}";
+
+    input = FetchSlices(sourceCode, "file.cpp");
+
+    output = "{\n"
+            "\"slice_0\" : {\n"
+            "    \"file\":\"file.cpp\",\n"
+            "    \"class\":\"\",\n"
+            "    \"function\":\"jar\",\n"
+            "    \"type\":\"int\",\n"
+            "    \"name\":\"rdx\",\n"
+            "    \"dependentVariables\": [  ],\n"
+            "    \"aliases\": [  ],\n"
+            "    \"calledFunctions\": [  ],\n"
+            "    \"use\": [ 2, 5 ],\n"
+            "    \"definition\": [ 1 ]\n"
+            "},\n"
+            "\"slice_1\" : {\n"
+            "    \"file\":\"file.cpp\",\n"
+            "    \"class\":\"\",\n"
+            "    \"function\":\"jar\",\n"
+            "    \"type\":\"int\",\n"
+            "    \"name\":\"rdp\",\n"
+            "    \"dependentVariables\": [  ],\n"
+            "    \"aliases\": [  ],\n"
+            "    \"calledFunctions\": [  ],\n"
+            "    \"use\": [ 3, 5 ],\n"
+            "    \"definition\": [ 1 ]\n"
+            "}\n"
+            "}\n"
+
+    testStatus = (strcmp(input.c_str(), output.c_str()) == 0);
+    DebugOutput(verbose, testStatus, "Const-Reference Test 8", input, output);
+
+    REQUIRE( strcmp(input.c_str(), output.c_str()) == 0 );
+}
+
+TEST_CASE( "Const-Reference Test 9", "[srcslice]" ) {
+    sourceCode = "void jar(const int& rdp, const int& rdx) {\n"
+                "    if (rdx > 5) {\n"
+                "        std::cout << rdp << std::endl;\n"
+                "    }\n"
+                "    std::cout << rdx + rdp << std::endl;\n"
+                "}\n"
+                "int main() {\n"
+                "    int reg = 10;\n"
+                "    int bsp = 3;\n"
+                "}\n";
+
+    input = FetchSlices(sourceCode, "file.cpp");
+
+    output = "{\n"
+            "\"slice_0\" : {\n"
+            "    \"file\":\"file.cpp\",\n"
+            "    \"class\":\"\",\n"
+            "    \"function\":\"main\",\n"
+            "    \"type\":\"int\",\n"
+            "    \"name\":\"bsp\",\n"
+            "    \"dependentVariables\": [  ],\n"
+            "    \"aliases\": [  ],\n"
+            "    \"calledFunctions\": [  ],\n"
+            "    \"use\": [  ],\n"
+            "    \"definition\": [ 9 ]\n"
+            "},\n"
+            "\"slice_1\" : {\n"
+            "    \"file\":\"file.cpp\",\n"
+            "    \"class\":\"\",\n"
+            "    \"function\":\"main\",\n"
+            "    \"type\":\"int\",\n"
+            "    \"name\":\"reg\",\n"
+            "    \"dependentVariables\": [  ],\n"
+            "    \"aliases\": [  ],\n"
+            "    \"calledFunctions\": [  ],\n"
+            "    \"use\": [  ],\n"
+            "    \"definition\": [ 8 ]\n"
+            "},\n"
+            "\"slice_2\" : {\n"
+            "    \"file\":\"file.cpp\",\n"
+            "    \"class\":\"\",\n"
+            "    \"function\":\"jar\",\n"
+            "    \"type\":\"int\",\n"
+            "    \"name\":\"rdx\",\n"
+            "    \"dependentVariables\": [  ],\n"
+            "    \"aliases\": [  ],\n"
+            "    \"calledFunctions\": [  ],\n"
+            "    \"use\": [ 2, 5 ],\n"
+            "    \"definition\": [ 1 ]\n"
+            "},\n"
+            "\"slice_3\" : {\n"
+            "    \"file\":\"file.cpp\",\n"
+            "    \"class\":\"\",\n"
+            "    \"function\":\"jar\",\n"
+            "    \"type\":\"int\",\n"
+            "    \"name\":\"rdp\",\n"
+            "    \"dependentVariables\": [  ],\n"
+            "    \"aliases\": [  ],\n"
+            "    \"calledFunctions\": [  ],\n"
+            "    \"use\": [ 3, 5 ],\n"
+            "    \"definition\": [ 1 ]\n"
+            "}\n"
+            "}\n"
+
+    testStatus = (strcmp(input.c_str(), output.c_str()) == 0);
+    DebugOutput(verbose, testStatus, "Const-Reference Test 9", input, output);
+
+    REQUIRE( strcmp(input.c_str(), output.c_str()) == 0 );
+}
+
+TEST_CASE( "Const-Reference Test 10", "[srcslice]" ) {
+    sourceCode = "void jar(const int& rdp, const int& rdx) {\n"
+                "    if (rdx > 5) {\n"
+                "        std::cout << rdp << std::endl;\n"
+                "    }\n"
+                "    std::cout << rdx + rdp << std::endl;\n"
+                "}\n"
+                "int main() {\n"
+                "    int reg = 10;\n"
+                "    int bsp = 3;\n"
+                "    jar(reg, bsp);\n"
+                "}\n";
+
+    input = FetchSlices(sourceCode, "file.cpp");
+
+    output = "{\n"
+            "\"slice_0\" : {\n"
+            "    \"file\":\"file.cpp\",\n"
+            "    \"class\":\"\",\n"
+            "    \"function\":\"main\",\n"
+            "    \"type\":\"int\",\n"
+            "    \"name\":\"bsp\",\n"
+            "    \"dependentVariables\": [  ],\n"
+            "    \"aliases\": [  ],\n"
+            "    \"calledFunctions\": [ {\"functionName\": \"jar\", \"parameterNumber\": \"2\"} ],\n"
+            "    \"use\": [ 1, 2, 5, 10 ],\n"
+            "    \"definition\": [ 9 ]\n"
+            "},\n"
+            "\"slice_1\" : {\n"
+            "    \"file\":\"file.cpp\",\n"
+            "    \"class\":\"\",\n"
+            "    \"function\":\"main\",\n"
+            "    \"type\":\"int\",\n"
+            "    \"name\":\"reg\",\n"
+            "    \"dependentVariables\": [  ],\n"
+            "    \"aliases\": [  ],\n"
+            "    \"calledFunctions\": [ {\"functionName\": \"jar\", \"parameterNumber\": \"1\"} ],\n"
+            "    \"use\": [ 1, 3, 5, 10 ],\n"
+            "    \"definition\": [ 8 ]\n"
+            "},\n"
+            "\"slice_2\" : {\n"
+            "    \"file\":\"file.cpp\",\n"
+            "    \"class\":\"\",\n"
+            "    \"function\":\"jar\",\n"
+            "    \"type\":\"int\",\n"
+            "    \"name\":\"rdx\",\n"
+            "    \"dependentVariables\": [  ],\n"
+            "    \"aliases\": [  ],\n"
+            "    \"calledFunctions\": [  ],\n"
+            "    \"use\": [ 2, 5 ],\n"
+            "    \"definition\": [ 1 ]\n"
+            "},\n"
+            "\"slice_3\" : {\n"
+            "    \"file\":\"file.cpp\",\n"
+            "    \"class\":\"\",\n"
+            "    \"function\":\"jar\",\n"
+            "    \"type\":\"int\",\n"
+            "    \"name\":\"rdp\",\n"
+            "    \"dependentVariables\": [  ],\n"
+            "    \"aliases\": [  ],\n"
+            "    \"calledFunctions\": [  ],\n"
+            "    \"use\": [ 3, 5 ],\n"
+            "    \"definition\": [ 1 ]\n"
+            "}\n"
+            "}\n"
+
+    testStatus = (strcmp(input.c_str(), output.c_str()) == 0);
+    DebugOutput(verbose, testStatus, "Const-Reference Test 10", input, output);
 
     REQUIRE( strcmp(input.c_str(), output.c_str()) == 0 );
 }
