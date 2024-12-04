6ee371c6e36567c5b3034630d16fa83f61bb21e4
Ender <nallahabi2003@gmail.com>
2024-12-04 18:14:58 -0500

    Created a testcase focusing on new aliases output

---
+++ test/testAliases.cpp
@@ -0,0 +1,187 @@
+#define CATCH_CONFIG_MAIN
+#include "./srcSliceTest.hpp"
+
+std::string sourceCode = "", input = "", output = "";
+bool testStatus = false;
+
+TEST_CASE( "Aliases Test 1", "[srcslice]" ) {
+    sourceCode = "int main() {\n"
+                "    int a = 3;\n"
+                "    int* x = &a;\n"
+                "}";
+
+    input = FetchSlices(sourceCode.c_str(), "file.cpp");
+
+    output = "{\n"
+            "\"slice_0\":{\n"
+            "    \"file\":\"file.cpp\",\n"
+            "    \"language\":\"C++\",\n"
+            "    \"namespace\":[],\n"
+            "    \"class\":\"\",\n"
+            "    \"function\":\"main\",\n"
+            "    \"type\":\"int*\",\n"
+            "    \"name\":\"x\",\n"
+            "    \"dependence\":[],\n"
+            "    \"aliases\":[{\"a\":3}],\n"
+            "    \"calls\":[],\n"
+            "    \"use\":[],\n"
+            "    \"definition\":[3]\n"
+            "},\n"
+            "\"slice_1\":{\n"
+            "    \"file\":\"file.cpp\",\n"
+            "    \"language\":\"C++\",\n"
+            "    \"namespace\":[],\n"
+            "    \"class\":\"\",\n"
+            "    \"function\":\"main\",\n"
+            "    \"type\":\"int\",\n"
+            "    \"name\":\"a\",\n"
+            "    \"dependence\":[],\n"
+            "    \"aliases\":[],\n"
+            "    \"calls\":[],\n"
+            "    \"use\":[3],\n"
+            "    \"definition\":[2]\n"
+            "}\n"
+            "}\n";
+
+    testStatus = (strcmp(input.c_str(), output.c_str()) == 0);
+    DebugOutput(testStatus, "Aliases Test 1", input, output, sourceCode);
+
+    REQUIRE( strcmp(input.c_str(), output.c_str()) == 0 );
+}
+
+TEST_CASE( "Aliases Test 2", "[srcslice]" ) {
+    sourceCode = "int main() {\n"
+                "    int a = 3;\n"
+                "    int b = 13;\n"
+                "    int* x = &a;\n"
+                "    x = &b;\n"
+                "}";
+
+    input = FetchSlices(sourceCode.c_str(), "file.cpp");
+
+    output = "{\n"
+            "\"slice_0\":{\n"
+            "    \"file\":\"file.cpp\",\n"
+            "    \"language\":\"C++\",\n"
+            "    \"namespace\":[],\n"
+            "    \"class\":\"\",\n"
+            "    \"function\":\"main\",\n"
+            "    \"type\":\"int*\",\n"
+            "    \"name\":\"x\",\n"
+            "    \"dependence\":[],\n"
+            "    \"aliases\":[{\"a\":4},{\"b\":5}],\n"
+            "    \"calls\":[],\n"
+            "    \"use\":[],\n"
+            "    \"definition\":[4,5]\n"
+            "},\n"
+            "\"slice_1\":{\n"
+            "    \"file\":\"file.cpp\",\n"
+            "    \"language\":\"C++\",\n"
+            "    \"namespace\":[],\n"
+            "    \"class\":\"\",\n"
+            "    \"function\":\"main\",\n"
+            "    \"type\":\"int\",\n"
+            "    \"name\":\"b\",\n"
+            "    \"dependence\":[],\n"
+            "    \"aliases\":[],\n"
+            "    \"calls\":[],\n"
+            "    \"use\":[5],\n"
+            "    \"definition\":[3]\n"
+            "},\n"
+            "\"slice_2\":{\n"
+            "    \"file\":\"file.cpp\",\n"
+            "    \"language\":\"C++\",\n"
+            "    \"namespace\":[],\n"
+            "    \"class\":\"\",\n"
+            "    \"function\":\"main\",\n"
+            "    \"type\":\"int\",\n"
+            "    \"name\":\"a\",\n"
+            "    \"dependence\":[],\n"
+            "    \"aliases\":[],\n"
+            "    \"calls\":[],\n"
+            "    \"use\":[4],\n"
+            "    \"definition\":[2]\n"
+            "}\n"
+            "}\n";
+
+    testStatus = (strcmp(input.c_str(), output.c_str()) == 0);
+    DebugOutput(testStatus, "Aliases Test 2", input, output, sourceCode);
+
+    REQUIRE( strcmp(input.c_str(), output.c_str()) == 0 );
+}
+
+TEST_CASE( "Aliases Test 3", "[srcslice]" ) {
+    sourceCode = "int main() {\n"
+                "    int a = 3;\n"
+                "    int b = 13;\n"
+                "    int* x = &a;\n"
+                "    x = &b;\n"
+                "    int* y = x;\n"
+                "}";
+
+    input = FetchSlices(sourceCode.c_str(), "file.cpp");
+
+    output = "{\n"
+            "\"slice_0\":{\n"
+            "    \"file\":\"file.cpp\",\n"
+            "    \"language\":\"C++\",\n"
+            "    \"namespace\":[],\n"
+            "    \"class\":\"\",\n"
+            "    \"function\":\"main\",\n"
+            "    \"type\":\"int*\",\n"
+            "    \"name\":\"y\",\n"
+            "    \"dependence\":[],\n"
+            "    \"aliases\":[{\"a\":4},{\"b\":5},{\"x\":6}],\n"
+            "    \"calls\":[],\n"
+            "    \"use\":[],\n"
+            "    \"definition\":[6]\n"
+            "},\n"
+            "\"slice_1\":{\n"
+            "    \"file\":\"file.cpp\",\n"
+            "    \"language\":\"C++\",\n"
+            "    \"namespace\":[],\n"
+            "    \"class\":\"\",\n"
+            "    \"function\":\"main\",\n"
+            "    \"type\":\"int*\",\n"
+            "    \"name\":\"x\",\n"
+            "    \"dependence\":[],\n"
+            "    \"aliases\":[{\"a\":4},{\"b\":5}],\n"
+            "    \"calls\":[],\n"
+            "    \"use\":[6],\n"
+            "    \"definition\":[4,5]\n"
+            "},\n"
+            "\"slice_2\":{\n"
+            "    \"file\":\"file.cpp\",\n"
+            "    \"language\":\"C++\",\n"
+            "    \"namespace\":[],\n"
+            "    \"class\":\"\",\n"
+            "    \"function\":\"main\",\n"
+            "    \"type\":\"int\",\n"
+            "    \"name\":\"b\",\n"
+            "    \"dependence\":[],\n"
+            "    \"aliases\":[],\n"
+            "    \"calls\":[],\n"
+            "    \"use\":[5],\n"
+            "    \"definition\":[3]\n"
+            "},\n"
+            "\"slice_3\":{\n"
+            "    \"file\":\"file.cpp\",\n"
+            "    \"language\":\"C++\",\n"
+            "    \"namespace\":[],\n"
+            "    \"class\":\"\",\n"
+            "    \"function\":\"main\",\n"
+            "    \"type\":\"int\",\n"
+            "    \"name\":\"a\",\n"
+            "    \"dependence\":[],\n"
+            "    \"aliases\":[],\n"
+            "    \"calls\":[],\n"
+            "    \"use\":[4],\n"
+            "    \"definition\":[2]\n"
+            "}\n"
+            "}\n";
+
+    testStatus = (strcmp(input.c_str(), output.c_str()) == 0);
+    DebugOutput(testStatus, "Aliases Test 3", input, output, sourceCode);
+
+    REQUIRE( strcmp(input.c_str(), output.c_str()) == 0 );
+}
