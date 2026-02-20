8bbca72cf069287b74ed9ca993625d662a7d76c0
Ender <nallahabi2003@gmail.com>
2026-02-19 22:24:03 -0500

    Removed unused and irregular segments

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -24,15 +24,15 @@ int main(int argc, char **argv)
 
     CLI::App app{"srcSlice (srcML Slicing Tool)"};
     // Options hold extra data
-    app.add_option ("-i, --input",   inputFile,                 "Name of the srcML input file [Must be built using the --position and --hash flags]")
+    app.add_option  ("-i, --input",   inputFile,                    "Name of the srcML input file [Must be built using the --position and --hash flags]")
         ->required()
         ->type_name("srcML FILE");
-    app.add_flag ("-c, --control-edges", showControlEdges,      "Display Control-Edges of the Slice");
-    app.add_option ("-o, --output",  outputFile,                "Name of the JSON output file [Stdout is Default]");
-    app.add_option ("-t, --threads", threadCount,               ss.str())
+    app.add_flag    ("-c, --control-edges", showControlEdges,       "Display Control-Edges of the Slice");
+    app.add_option  ("-o, --output",  outputFile,                   "Name of the JSON output file [Stdout is Default]");
+    app.add_option  ("-t, --threads", threadCount,               ss.str())
         ->default_val(threadCount);
-    app.add_flag ("-v, --verbose", debugMode,                   "Display Debug Info when Slicing");
-    app.add_flag ("-p, --progress", feedbackMode,               "Display Feedback Progress Bars");
+    app.add_flag    ("-v, --verbose", debugMode,                    "Display Debug Info when Slicing");
+    app.add_flag    ("-p, --progress", feedbackMode,                "Display Feedback Progress Bars");
     
     CLI11_PARSE(app, argc, argv);
 
@@ -53,29 +53,15 @@ int main(int argc, char **argv)
             outFile.close();
         }
 
-        std::map<std::string, unsigned int> sliceProfileNames;
-
         // opening of the entire JSON object
         sliceOutput << "{" << std::endl;
 
-        for (auto& profile : sliceProfileMap)
-        {
+        for (auto& profile : sliceProfileMap) {
             ++currIndex;
             
-            for (auto& slice : profile.second)
-            {
-                if (slice.containsDeclaration)
-                {
-                    // write out the start of the json object
-                    // build json slice profile name
-                    // variableName_initDefLine_checksum_numericalTagger
-
+            for (auto& slice : profile.second) {
+                if (slice.containsDeclaration) {
                     std::string name(slice.variableName + '-' + slice.initialPosition.ToNameString() + '-' + slice.checksum);
-                    ++sliceProfileNames[name]; // if the name exists in the map we will see the value increase
-                    // if the numeric tag after substracting 1 is not 0 we need to include this tag
-                    // in the slice name when we pipe to stdout
-                    if (sliceProfileNames[name]-1 != 0) name += '-' + sliceProfileNames[name];
-
                     sliceOutput << "\"" << name << "\":{" << std::endl;
 
                     // print out content of the SliceProfile
@@ -85,7 +71,7 @@ int main(int argc, char **argv)
                     if (currIndex != totalElements)
                         sliceOutput << "}," << std::endl;
                     else
-                        sliceOutput << "}___" << std::endl;
+                        sliceOutput << "}" << std::endl;
                 }
             }
         }
